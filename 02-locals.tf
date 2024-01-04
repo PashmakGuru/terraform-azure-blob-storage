ca2df52eeb28160578a525053ead2f3908c60eb9
AmirrezaNasiri <AmirrezaNasiri@users.noreply.github.com>
2024-01-04 20:16:31 +0000

    🔨 Run `terraform fmt`

--- 02-locals.tf
+++ 02-locals.tf
@@ -2,11 +2,11 @@ locals {
   id = "${var.name}-${var.environment}"
   common_tags = {
     module = "blob-storage"
-    name = var.name
-    env = var.environment
+    name   = var.name
+    env    = var.environment
   }
   rbac = {
-    for r in var.rbac:
-        "${r.identity_resource_group}-${r.identity_name}" => r
+    for r in var.rbac :
+    "${r.identity_resource_group}-${r.identity_name}" => r
   }
 }
